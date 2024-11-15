608545534e94736e0bad4d395200b9c90dbf89bb
Corneliu Ciugurean <153821557+Ciugiu@users.noreply.github.com>
2024-11-15 01:38:41 +0100

    Update main.js
    netlify uses dotenv

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const fetchData = async (query) => {
   const options = {
     method: "GET",
     headers: {
-      "x-rapidapi-key": import.meta.env.VITE_RAPID_KEY,
+      "x-rapidapi-key": process.env.VITE_RAPID_KEY,
       "x-rapidapi-host": "epic-games-store.p.rapidapi.com",
     },
   };
